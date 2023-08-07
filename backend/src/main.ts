d41c10b8feaff704f4a5f8d2c97634ef514a0d3d
chidi-godwin <chidieberen1998@gmail.com>
2023-08-07 09:34:55 +0100

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,7 @@ import { CustomExceptionFilter } from './filters/error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({ origin: '*' });
 
   // enable global validation
   app.useGlobalPipes(
